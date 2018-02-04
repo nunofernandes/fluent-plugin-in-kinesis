d6ef12a72d89ac2bda95e7ff6952c05671456c4b
saorio <saori@SaorinoMacBook-Air.local>
2018-02-04 23:24:35 +0900

    test修正

--- test/plugin/test_in_kinesis.rb
+++ test/plugin/test_in_kinesis.rb
@@ -56,7 +56,9 @@ class KinesisInputTest < Test::Unit::TestCase
     stub(Aws::Kinesis::Client).new do |options|
       assert_equal("test_key_id", options[:access_key_id])
       assert_equal("test_sec_key", options[:secret_access_key])
-      assert_equal("us-east-1", options[:region])
+      assert_equal("ap-northeast-1", options[:region])
+      assert_equal("test_stream", options[:stream_name])
+      assert_equal("/var/log/in_kinesis", options[:state_dir_path])
       client
     end
 
@@ -71,52 +73,23 @@ class KinesisInputTest < Test::Unit::TestCase
     stub(Aws::Kinesis::Client).new do |options|
       assert_equal(nil, options[:access_key_id])
       assert_equal(nil, options[:secret_access_key])
-      assert_equal("us-east-1", options[:region])
+      assert_equal("ap-northeast-1", options[:region])
 
       credentials = options[:credentials]
       assert_equal("default", credentials.profile_name)
-      assert_equal("/home/scott/.aws/credentials", credentials.path)
+      assert_equal("/home/foo/.aws/credentials", credentials.path)
 
       client
     end
 
     d = create_driver(<<-EOS)
       profile default
-      credentials_path /home/scott/.aws/credentials
+      credentials_path /home/foo/.aws/credentials
       stream_name test_stream
-      region us-east-1
-      fixed_partition_key test_partition_key
-      buffer_chunk_limit 100k
+      region ap-northeast-1
+      state_dir_path /var/log/in_kinesis
     EOS
 
-    d.run(default_tag: "test")
-  end
-
-  def test_load_client_with_role_arn
-    client = stub(Object.new)
-    client.put_record { {} }
-
-    stub(Aws::AssumeRoleCredentials).new do |options|
-      assert_equal("arn:aws:iam::001234567890:role/my-role", options[:role_arn])
-      assert_equal("fluent-plugin-kinesis-aggregation", options[:role_session_name])
-      assert_equal("my_external_id", options[:external_id])
-      assert_equal(3600, options[:duration_seconds])
-      "sts_credentials"
-    end
-
-    stub(Aws::Kinesis::Client).new do |options|
-      assert_equal("sts_credentials", options[:credentials])
-      client
-    end
-
-    d = create_driver(<<-EOS)
-      role_arn arn:aws:iam::001234567890:role/my-role
-      external_id my_external_id
-      stream_name test_stream
-      region us-east-1
-      fixed_partition_key test_partition_key
-      buffer_chunk_limit 100k
-    EOS
     d.run(default_tag: "test")
   end
 end
